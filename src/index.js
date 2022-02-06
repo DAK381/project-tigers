af0c15c4be23979f0e11f01ec7f2c68f310ae782
LOWEKA <97915602+LOWEKA@users.noreply.github.com>
2022-02-05 20:41:19 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
-
-<BrowserRouter><App />
-</BrowserRouter>, 
-document.getElementById('root')
-);
-
-
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, 
+document.getElementById('root'));
