fbf6a6a1710eb2e0af4bed25f1f4324765e81077
Daniel Kennedy <72562742+DAK381@users.noreply.github.com>
2022-02-06 19:15:19 -0600

    Revert "Add files via upload"

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import './index.css';
 import App from './App';
-import "bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter } from 'react-router-dom';
+
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
-document.getElementById('root'));
+
+<BrowserRouter><App />
+</BrowserRouter>, 
+document.getElementById('root')
+);
+
+
