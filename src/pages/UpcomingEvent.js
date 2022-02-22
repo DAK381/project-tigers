273f8ad5309f2948f6e3e05e749b4e131c85ccd1
supra394 <60794557+supra394@users.noreply.github.com>
2022-02-22 00:53:44 -0600

    Update UpcomingEvent.js

--- src/pages/UpcomingEvent.js
+++ src/pages/UpcomingEvent.js
@@ -1,60 +1,59 @@
+import Slider from "../component/layout/Slider";
+import { Link } from "react-router-dom";
+import UpcomingEvent from "./UpcomingEvent";
 import CardGroup from 'react-bootstrap/CardGroup'
 import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
-function UpcomingEvent(){
+function Home() {
 
-    return( 
-        <div>
-            <CardGroup>
-  <Card>
-    <Card.Img va driant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-       s This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Added ....</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Added...</small>
-    </Card.Footer>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>Card title</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
+    return (
         
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Added....</small>
-    </Card.Footer>
-  </Card>
-</CardGroup>
-        </div>
+        
+       <div className = "body">
+        <Slider />
+        <Container>
+        <section className = "section">
+             
 
 
+                    <div classname = "col-md-12 text center">
+                        <h3 className = "heading">WELCOME</h3>
 
+                        <div className = "underline"></div>
 
+                        <p className = "description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+                    </div>
+
+           {/* <div className = "underline2"></div> */}
+ 
+           
+            <div className = "container">
+                
+                        <h3 className = "heading">Upcoming Events</h3>
+
+                        <div className = "underline"></div>
+
+                       
+                            <UpcomingEvent />
+                          
+                        
+                    </div>
+                
+            </section>
+    
+          
+            
+        
+            
+           
+            </Container>
+            </div>
+            
     )
-
 }
 
-export default UpcomingEvent;
+export default Home;
