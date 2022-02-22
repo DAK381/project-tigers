f0f391bd74f87928d09756172bdae3c8c873784e
supra394 <60794557+supra394@users.noreply.github.com>
2022-02-22 00:55:29 -0600

    Update UpcomingEvent.js

--- src/pages/UpcomingEvent.js
+++ src/pages/UpcomingEvent.js
@@ -1,59 +1,95 @@
-import Slider from "../component/layout/Slider";
-import { Link } from "react-router-dom";
-import UpcomingEvent from "./UpcomingEvent";
 import CardGroup from 'react-bootstrap/CardGroup'
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+import photo1 from "../Images/photo1.jpeg"
+import photo2 from "../Images/photo2.jpeg"
+import photo3 from "../Images/photo3.jpeg"
+
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
-function Home() {
 
-    return (
-        
-        
-       <div className = "body">
-        <Slider />
-        <Container>
-        <section className = "section">
-             
+function UpcomingEvent(){
 
+    return( 
 
-                    <div classname = "col-md-12 text center">
-                        <h3 className = "heading">WELCOME</h3>
-
-                        <div className = "underline"></div>
-
-                        <p className = "description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-
-                    </div>
-
-           {/* <div className = "underline2"></div> */}
- 
-           
-            <div className = "container">
-                
-                        <h3 className = "heading">Upcoming Events</h3>
-
-                        <div className = "underline"></div>
-
-                       
-                            <UpcomingEvent />
-                          
-                        
-                    </div>
-                
-            </section>
+      <Container>
+        <div>
+            <CardGroup>
+  <Row>
+    <Col md>
+  <Card className = "mb-3">
+    <Card.Img va driant="top" src= {photo1} />
+    <Card.Body>
+      <Card.Title>EVENT 1</Card.Title>
+      <Card.Text>
+      Description
+      </Card.Text>
+    </Card.Body>
+    <Button variant = "primary"> Read More</Button>
+    <Card.Footer>
     
-          
-            
+      <small className="text-muted">Added 5 days ago</small>
+    </Card.Footer>
+
+    
+  </Card>
+  </Col>
+  </Row>
+
+
+  <Row>
+    <Col md>
+  <Card className = "mb-3">
+    <Card.Img variant="top" src= {photo1} />
+    <Card.Body>
+      <Card.Title>EVENT 2</Card.Title>
+      <Card.Text>
+        Description
+      </Card.Text>
+    </Card.Body>
+    <Button variant = "primary"> Read More</Button>
+
+    <Card.Footer>
+      <small className="text-muted">Added 9 days ago</small>
+    </Card.Footer>
+    
+  </Card>
+
+  </Col>
+  </Row>
+
+
+  <Row>
+    <Col md>
+  <Card className = "mb-3">
+    <Card.Img variant="top" src= {photo1} />
+    <Card.Body>
+      <Card.Title>Event 3</Card.Title>
+      <Card.Text>
+        Description
         
-            
-           
-            </Container>
-            </div>
-            
+      </Card.Text>
+    </Card.Body>
+    <Button variant = "primary"> Read More</Button>
+    <Card.Footer>
+    
+      <small className="text-muted">Added 10 days ago</small>
+    </Card.Footer>
+  </Card>
+  </Col>
+  </Row>
+
+</CardGroup>
+        </div>
+        </Container>
+
+
+
+
     )
+
 }
 
-export default Home;
+export default UpcomingEvent;
