bd97ba8e8d0e25ce60893148c7dcc793b7adefbf
supra394 <60794557+supra394@users.noreply.github.com>
2022-02-22 00:54:10 -0600

    Update Footer.js

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,20 +1,35 @@
-import Card from 'react-bootstrap/Card'
+import React from 'react';
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
-function Footer (){
+function Footer() {
+  return (
+    
+    <div className='footer-container'>
+      <section className='footer-heading'>
+        <p className='footer-heading'>
+          JOIN THE NAFA TODAY!
+        </p>
+        <p className='footer-heading-text'>
+        <Link to='sign-up'> Be a part of the Neville Family. </Link>
+        </p>
 
-    return(
-        <div>
-            <Card>
-            <Card.Footer className="text-muted">
-
-                <div className = "footer">
-                    <p>Footer content</p>
-                    </div>
-
-            </Card.Footer>
-            </Card>
+      </section>
+      <div class='footer-links'>
+        
+          <div class='footer-link-items'>
+            <h2>Stay Connected</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+        
         </div>
-    )
+      </div>
+
+     
+    </div>
+  );
 }
 
 export default Footer;
