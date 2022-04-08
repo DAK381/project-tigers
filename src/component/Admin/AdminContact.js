03c7c4f3b32033940a9c7b1386c0b626f5fe3134
unknown <nischalp14@gmail.com>
2022-04-08 01:46:27 -0500

    Added forms to send email

--- src/component/Admin/AdminContact.js
+++ src/component/Admin/AdminContact.js
@@ -1,17 +1,59 @@
 import AdminNavigation from "./AdminNavigation";
-import { Container } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
+import { CardBody } from "reactstrap";
+import { Form, Button, FloatingLabel } from "react-bootstrap"
 
-function AdminContact(){
+function AdminContact() {
 
-    return(
+    return (
 
         <Container>
 
-        
-        <AdminNavigation />
-        
+
+            <AdminNavigation />
+
+            <Container>
+                <Card>
+                    <CardBody>
+                        <Card.Title>
+                            <h3>Contact Memerbs</h3></Card.Title>
+                        <Form>
+                            
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Label>Subject</Form.Label>
+                                <Form.Control type="text" placeholder="Email Subject" />
+
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Subject</Form.Label>
+                            <FloatingLabel controlId="floatingTextarea2" label="Comments">
+                            <Form.Control
+                            as="textarea"
+                            placeholder="Leave a comment here"
+                            style={{ height: '100px' }}
+                            />
+                            </FloatingLabel>
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit">
+                                Send
+                            </Button>
+                        </Form>
+                    </CardBody>
+                </Card>
+            </Container>
 
         </Container>
+
+        // contact members page
+
     )
 }
 
