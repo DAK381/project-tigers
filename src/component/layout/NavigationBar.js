a89823b7a5e63083b003d6e5e2bdb368cf26b7d4
unknown <nischalp14@gmail.com>
2022-04-11 02:47:55 -0500

    updated navigation bar

--- src/component/layout/NavigationBar.js
+++ src/component/layout/NavigationBar.js
@@ -54,42 +54,6 @@ return (
   </Container>
 </Navbar>
 
-// navigation end
-
-/*{ <Navbar bg="light" expand={false}>
-  <Container fluid>
-  <Navbar.Brand>
-     <img src = {logo} ahref = "./" alt = ""  width = "120x" height = "100px"/>
-          {' '}
-</Navbar.Brand>
-
-    <Navbar.Toggle aria-controls="offcanvasNavbar" />
-    <Navbar.Offcanvas
-      id="offcanvasNavbar"
-      aria-labelledby="offcanvasNavbarLabel"
-      placement="end"
-    >
-      <Offcanvas.Header closeButton>
-        <Offcanvas.Title id="offcanvasNavbarLabel"><h1> Menu </h1></Offcanvas.Title>
-      </Offcanvas.Header>
-      <Offcanvas.Body bg="dark">
-        <Nav className="justify-content-end flex-grow-1 pe-3">
-        <Nav.Link href="./"> <h2> Home </h2></Nav.Link>
-     <Nav.Link href="events"><h2>Events</h2></Nav.Link>
-     <Nav.Link href="scholarship"><h2> Scholarships </h2></Nav.Link>
-     { !token && <Nav.Link href="log-in"> <h2> Log In </h2></Nav.Link> }
-     { !token && <Nav.Link href="sign-up"><h2>Sign Up </h2></Nav.Link> }
-     <Nav.Link href="contact-us"><h2>Contact Us </h2></Nav.Link>
-     { userData.role === "ADMIN" && <Nav.Link href="admin"><h2> Admin </h2></Nav.Link> }
-     { token && userData.role !== "ADMIN" && <Nav.Link href="user-profile"><h2>Profile</h2></Nav.Link> }
-     { token && <Nav.Link href="./" onClick={() =>logOut()}><h2>Logout</h2></Nav.Link> }
-
-        </Nav>
-      </Offcanvas.Body>
-    </Navbar.Offcanvas>
-  </Container>
-</Navbar> }*/
-
 
 
 )
