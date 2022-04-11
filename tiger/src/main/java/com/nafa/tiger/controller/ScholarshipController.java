c92c2435d8357f0131a13b01a55dceb6ff6b99b2
Daniel Kennedy <72562742+DAK381@users.noreply.github.com>
2022-04-11 01:18:41 -0500

    Added some cors annotations

--- tiger/src/main/java/com/nafa/tiger/controller/ScholarshipController.java
+++ tiger/src/main/java/com/nafa/tiger/controller/ScholarshipController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.ArrayList;
 
@@ -16,6 +17,8 @@ public class ScholarshipController {
 
     @Autowired
     private ScholarshipService scholarshipService;
+
+    @CrossOrigin("*")
     @GetMapping("/get-all-scholarship")
     public ArrayList<Scholarship> getAllScholarship(){
         return scholarshipService.getAllScholarship();
