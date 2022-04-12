8110c1d9544877d61718d8cf426a25257a9dc054
Daniel Kennedy <72562742+DAK381@users.noreply.github.com>
2022-04-11 23:08:16 -0500

    Fixed file
    File was messed up in merge so I just pasted my old version over it instead.

--- src/component/layout/NavigationBar.js
+++ src/component/layout/NavigationBar.js
@@ -20,34 +20,42 @@ function NavigationBar(props){
 
   }
 
+return (
 
-//  new design for the navigation bar
+<div>
 
+{ userData.role !== "ADMIN" && 
+<Navbar bg="light" expand={false} >
+  <Container fluid>
+    <Navbar.Brand>
+     <img src = {logo} ahref = "./" alt = ""  width = "120x" height = "100px"/>
+          {' '}
+    </Navbar.Brand>
 
+    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+    <Navbar.Offcanvas
+      id="offcanvasNavbar"
+      aria-labelledby="offcanvasNavbarLabel"
+      placement="end"
+    >
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title id="offcanvasNavbarLabel"><h1> Menu </h1></Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body bg="dark">
+        <Nav className="justify-content-end flex-grow-1 pe-3" >
+        <Nav.Link href="./"> <h2> Home </h2></Nav.Link>
+        <Nav.Link href="events"><h2>Events</h2></Nav.Link>
+        <Nav.Link href="scholarship"><h2> Scholarships </h2></Nav.Link>
+        { !token && <Nav.Link href="log-in"> <h2> Log In </h2></Nav.Link> }
+        { !token && <Nav.Link href="sign-up"><h2>Sign Up </h2></Nav.Link> }
+        <Nav.Link href="contact-us"><h2>Contact Us </h2></Nav.Link>
+        {/* userData.role === "ADMIN" && <Nav.Link href="admin"><h2> Admin </h2></Nav.Link> */}
+        { token && userData.role !== "ADMIN" && <Nav.Link href="user-profile"><h2>Profile</h2></Nav.Link> }
+        { token && <Nav.Link href="./" onClick={() =>logOut()}><h2>Logout</h2></Nav.Link> }
 
-<Navbar bg="dark" variant="dark"  expand="lg" >
-  <Container>
-  <Navbar.Brand href="./">
-  <img src = {logo} ahref = "./" alt = ""  width = "9%" />
-          {' '}  NAFA
-  </Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse>
-      <Nav className="ms-auto">
-        <Nav.Link href="events" >Events</Nav.Link>
-        <Nav.Link href="scholarships">Scholarships</Nav.Link>
-        <NavDropdown title="User" id="basic-nav-dropdown">
-          <NavDropdown.Item  href="log-in">Login</NavDropdown.Item>
-          <NavDropdown.Item href="sign-up">Signup</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Divider />
-          { userData.role === "ADMIN" && <Nav.Link href="admin"><h2> Admin </h2></Nav.Link> }
-          { token && userData.role !== "ADMIN" && <Nav.Link href="user-profile"><h2>Profile</h2></Nav.Link> }
-     { token && <Nav.Link href="./" onClick={() =>logOut()}><h2>Logout</h2></Nav.Link> } 
-         
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
+        </Nav>
+      </Offcanvas.Body>
+    </Navbar.Offcanvas>
   </Container>
 </Navbar> }
 
