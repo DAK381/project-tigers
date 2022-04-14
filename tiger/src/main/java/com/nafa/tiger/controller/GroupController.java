08caddfdb66a3927489df65e806c57a2471c637b
Daniel Kennedy <72562742+DAK381@users.noreply.github.com>
2022-04-13 20:10:24 -0500

    Adding back changes
    adding back my changes to backend files

--- tiger/src/main/java/com/nafa/tiger/controller/GroupController.java
+++ tiger/src/main/java/com/nafa/tiger/controller/GroupController.java
@@ -9,10 +9,18 @@ import com.nafa.tiger.entity.Group;
 import com.nafa.tiger.entity.User;
 import com.nafa.tiger.service.GroupService;
 
+import java.sql.*;
+
 @RestController
 @CrossOrigin(origins ="*")
 public class GroupController {
+	
+	protected class MemberGroup{
+		public int groupId;
+		public String groupName;
 
+		public MemberGroup(){}
+	}
 
 	@Autowired
 	private GroupService groupService;
@@ -36,6 +44,50 @@ public class GroupController {
 	@GetMapping("/search/name/{groupName}")
 	public ArrayList<Group> getGroupByNameContaining(@PathVariable("groupName") String groupName){
 		return groupService.getGroupByNameContaining(groupName);
+	}
+	
+	@GetMapping("/search/membersGroups/{userId}")
+	public ArrayList<MemberGroup> getMemberGroups(@PathVariable("userId") Long userId){
+
+		String query = "SELECT groups.group_id, groups.group_name FROM groups JOIN user_group ON groups.group_id=user_group.group_id WHERE user_group.user_id = " + userId;
+		ArrayList<MemberGroup> membersGroups= new ArrayList<MemberGroup>();
+
+		try(Connection con = DriverManager.getConnection("jdbc:postgresql://nafa.cp4e12t7aiyi.us-east-2.rds.amazonaws.com/registration",
+		"tiger", "nafatiger"); 
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery(query);
+		) {
+			while(rs.next()){
+				MemberGroup g = new MemberGroup();
+				g.groupId = rs.getInt("group_id");
+				g.groupName = rs.getString("group_name");
+				membersGroups.add(g);
+			}
+		}
+		catch (SQLException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		return membersGroups;
+	}
+	
+	@PostMapping("addMemberIngroup/{groupName}/{userName}")
+	public void addMemberToGroup(@PathVariable("userName") Long userId, @PathVariable("groupName") Long groupId){
+		groupService.addUserToGroup(userId,groupId);
+	}
+
+	@PostMapping("removeFromGroup/{groupName}/{userName}")
+	public void removeFromGroup(@PathVariable("userName") Long userId, @PathVariable("groupName") Long groupId){
+		try(Connection con = DriverManager.getConnection("jdbc:postgresql://nafa.cp4e12t7aiyi.us-east-2.rds.amazonaws.com/registration",
+		"tiger", "nafatiger");) {
+			String query = "DELETE FROM user_group WHERE user_id = " + userId + " AND group_id = " + groupId;
+			PreparedStatement ps = con.prepareStatement(query);
+			ps.executeUpdate();
+	
+		 } catch (SQLException e) {
+			System.out.println("Error: " + e.getMessage());
+		 }
 	}
 	
    
