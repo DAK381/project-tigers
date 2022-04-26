6c7e4c07683b088fd3539117aa169f0d604df260
LOWEKA <97915602+LOWEKA@users.noreply.github.com>
2022-04-25 21:13:11 -0500

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { MDBContainer } from 'mdbreact';
+
+const AboutPage = () => {
+  return(
+  <><div>
+      <h1>About Us</h1>
+
+      <h5>Mission Statement</h5>
+
+      <p>It is the mission of NAFA to provide supplemental funding for programs or projects to enhance the quality of instructional delivery and student life, and to
+        promote excellence in higher education at NHS.
+        If you have any information or event that needs to be featured on this site, please contact Dana Jefferson, NAFA Executive Director.</p>
+    
+    </div>
+    
+    <div>
+      <h5>President's Message:</h5>
+
+      <p>Alumni and Friends are the backbone of the Neville Nation! The Neville Alumni and Friends Association support scholarships for our students, beautification projects for our school and outreach programs with many civic and community organizations.
+         We need you to make all of this happen. The cost to join is minimal with a diversity of membership options available – student, individual, couple, lifetime, etc. Come join us in keeping Neville High School strong!
+      </p>
+    
+    </div>
+    <p>Jay Traylor, Class of 1980 <br></br>
+       NAFA President 2017-2019</p>
+    <div>
+      
+      
+    </div></>
+  )};
+
+export default AboutPage;
