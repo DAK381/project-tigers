c1c52ea482ad83f4d4ecaac5d8c85a4bb10b1c8c
suprabha08 <suprabhapudasainy08@gmail.com>
2022-04-26 17:59:13 -0500

    modal in calednar

--- src/pages/EventCalendar.js
+++ src/pages/EventCalendar.js
@@ -4,6 +4,7 @@ import { useLocation } from 'react-router-dom'
 import { useState, useEffect, useCallback } from 'react';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { Container } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal'
 
 export default function EventCalendar(){
 
@@ -11,9 +12,14 @@ export default function EventCalendar(){
     
     const events = location.state.events;
 
-   
+    const [modalIsOpen,setModalIsOpen] = useState(false);
 
     const localizer = momentLocalizer(moment)
+
+    const [selectedEvent,setSelectedEvent] = useState()
+
+    const [selected,setSelected] = useState(false)
+
 
     // const allViews = Object
     // .keys(Calendar.Views)
@@ -30,16 +36,22 @@ export default function EventCalendar(){
         )
     }, [events]);
 
-    const handleSelectEvent = useCallback(
-        (event) => window.alert(event.title),
-        []
-      )
+
+    const handleSelectEvent  = useCallback((event) => {
+        setSelectedEvent(event);
+        console.log(event)
+        setModalIsOpen(true)
+        setSelected(true)
+      }, [events]);
+
+   
 
 console.log(events)
     return(
         <div>
             <Container>
 
+       
             
 
 <Calendar
@@ -49,14 +61,29 @@ console.log(events)
       endAccessor="end"
       defaultView={Views.MONTH}
       selectable
-      onSelectEvent={handleSelectEvent}
+      onSelectEvent={(e) => handleSelectEvent(e)}
 
     //   views={allViews}
 
       style={{ height: 800}}
     />
+
+
+ 
+
             
             </Container>
+
+          {selected &&
+
+                <Modal show={modalIsOpen} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+                    <Modal.Header closeButton>
+                    <Modal.Title>{selectedEvent.title}</Modal.Title>
+                </Modal.Header>
+
+                </Modal>
+          
+          } 
 
         </div>
     )
