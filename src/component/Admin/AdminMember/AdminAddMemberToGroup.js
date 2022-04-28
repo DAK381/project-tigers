a113cb76050560432c0a716b92427244907c3618
suprabha08 <suprabhapudasainy08@gmail.com>
2022-04-28 03:02:19 -0500

    adding members to group only works if they're not already in the group

--- src/component/Admin/AdminMember/AdminAddMemberToGroup.js
+++ src/component/Admin/AdminMember/AdminAddMemberToGroup.js
@@ -11,15 +11,17 @@ export default function AdminAddMemberToGroup(props){
     const navigate = new useNavigate();
     const [show, setShow] = useState(false);
 
-  const location = new useLocation();
+    const location = new useLocation();
 
     const users = location.state.arrayId;
     console.log(users);
   
-    const [id, selectedId] = useState();
+    const [id, selectedId] = useState();  //is when button clicked
+    const [selGrp, setGrp] = useState("");
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
 
+    // const[alreadyAddedGroup, setAlreadyAdded] = useState([[]])
     const [groups, setGroups] = useState([]);
   
     
@@ -28,31 +30,56 @@ export default function AdminAddMemberToGroup(props){
             setGroups(res.data)
         }).catch(err => console.log(err))
 
-        
-        
-    }, [props])
+        // users.map(
+        //   (userId) => {
+        //     axios.get(`/search/membersGroups/${userId}`).then(res => {
+              
+              
+        //       var alreadyAddedGroup = res.data;
+        //       console.log(alreadyAddedGroup)
+
+        //       // if(alreadyAddedGroup.length !== 0){
+
+        //       //   alreadyAddedGroup.map(
+        //       //     (current) =>
+        //       //     {
+        //       //       console.log(userId, current.groupId)
+        //       //       if(current.groupId === selGrp){
+        //       //         alert("Selected members are already in the selected group. You can't add them to the same group")
+        //       //       }
+        //       //     }
+        //       //   )
+        //       // }
+
+
+        //   }).catch(err => console.log(err))
+        //   }
+        // )
+
+    }, [])
 
     const addMembers = () => {
       console.log(selectedId)
 
+      if(selectedId === null){
+        alert("Please select a group to add the members to.")
+      }
+    
+      
       users.map(
         (userId) =>
         {
-          console.log(userId, "....")
+          
           axios.put(`/addUserToGroup/${id}/${userId}`)
           .then(
             res => {
               console.log(userId, id)
-
-              navigate('admin-group-all')
+              navigate('/admin-group-all');
             }
             
           ).catch(err => console.log(err))
         }
       )
-
-     
-  
   }
 
   
