5f02c1da1a0addf3791269cd618c9551310de4a7
thapaashmin <2000ashmin@gmail.com>
2022-05-02 20:29:55 -0500

    recapcha

--- src/component/Forms/recaptcha/Captcha.js
+++ src/component/Forms/recaptcha/Captcha.js
@@ -1,16 +1,31 @@
 import React from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 
-const RecaptchaComponent = ({ handleRecapthca }) => {
+// const RecaptchaComponent = ({ handleRecapthca }) => {
 
   
+//   return (
+//     <ReCAPTCHA
+//       sitekey="6Lf5YrwfAAAAAALPFs3y30XZyxCoMJhUH6OoinS6"
+//       onChange={handleRecapthca}
+//       theme = "dark"
+//     />
+//   );
+// };
+
+// export default RecaptchaComponent;
+// import ReCAPTCHA from ‘react-google-recaptcha’
+const RecaptchaComponent = () => {
+  function onChange(value) {
+    console.log('Captcha value:', value);
+  }
   return (
-    <ReCAPTCHA
-      sitekey="6Lc8H-keAAAAAL5rCKgRycyv2NR2Shkx3MON-dni"
-      onChange={handleRecapthca}
-      theme = "dark"
-    />
+    <div className="RecaptchaComponent">
+      <ReCAPTCHA
+        sitekey="6Lesc7wfAAAAANkBrpuKvNbmQxWbjja3YoM0J8nE"
+        onChange={onChange}
+      />
+    </div>
   );
 };
-
 export default RecaptchaComponent;
